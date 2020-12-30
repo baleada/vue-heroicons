275bcad58b9f385c893d846101975f61d41ed8d3
AlexVipond <alex.r.vipond@gmail.com>
2020-12-30 11:57:04 -0600

    fixes gitignored tests

---
+++ tests/stubs/app/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <HeroiconsSun />
+  <HeroiconsSunOutline /> 
+</template>
+
+<script>
+import { HeroiconsSun, HeroiconsSunOutline } from '/@src/index.js'
+
+export default {
+  components: {
+    HeroiconsSun,
+    HeroiconsSunOutline,
+  },
+}
+</script>
